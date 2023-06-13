b33e8ce9773dd4ed83068c507965afaea2cb5f69
david <david@pbsoft.com.br>
2023-06-13 10:05:40 -0300

    Adicionado paginate.

--- app/Http/Livewire/ShowProdutos/Show.php
+++ app/Http/Livewire/ShowProdutos/Show.php
@@ -4,12 +4,16 @@ namespace App\Http\Livewire\ShowProdutos;
 
 use App\Models\Products;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Show extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
     public function render()
     {
-        $products = Products::all();
+        $products = Products::paginate(10);
         return view('livewire.show-produtos.show', compact('products'));
     }
 }
--- resources/views/livewire/show-produtos/show.blade.php
+++ resources/views/livewire/show-produtos/show.blade.php
@@ -1,12 +1,15 @@
 <div class="d-flex row justify-content-center">
-@foreach ($products as $product)
-    <div class="card m-3" style="width: 18rem;">
-        <img src="{{url($product->image)}}" class="card-img-top h-100" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card of the card'scontent.</p>
-            <a href="/ViewProduct/{1}" class="btn btn-secondary">Ver produto</a>
+    @foreach ($products as $product)
+        <div class="card m-3" style="width: 18rem;">
+            <img src="{{ url($product->image) }}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card of the card'scontent.</p>
+                <a href="/ViewProduct/{1}" class="btn btn-secondary">Ver produto</a>
+            </div>
         </div>
-    </div>
     @endforeach
+    <div class="pagination justify-content-center m-3">
+        {{ $products->links() }}
+    </div>
 </div>
